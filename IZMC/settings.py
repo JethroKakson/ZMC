38879fc9fae186d4ff9d11ad8354980b3361eed5
JethroKakson <jethrokakson@gmail.com>
2024-01-01 12:02:37 +0300

    collecting static

--- IZMC/settings.py
+++ IZMC/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, '../core/static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'core/static')
 STATIC_URL = 'static/'
-STATICFILES_DIR = (os.path.join(BASE_DIR, '../core/static'))
+STATICFILES_DIR = (os.path.join(BASE_DIR, 'core/static'))
 
 #this one applies when the user logs into thier account.
 LOGIN_REDIRECT_URL = 'home'
