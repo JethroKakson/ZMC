f1f879b5c6b36cf9a9c8b89a80e217461f514e60
JethroKakson <jethrokakson@gmail.com>
2024-01-15 16:25:57 +0300

    last commits 4

--- IZMC/settings.py
+++ IZMC/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&q^ea=a7%u68)k*068uut(@lqszi&a$h0q%y77tn+82-+-0(53'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-&q^ea=a7%u68)k*068uut(@lqszi&a$h0q%y77tn+82-+-0(53')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", 'True')=="True"
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'lets-seal.onrender.com']
 # ALLOWED_HOSTS = []
