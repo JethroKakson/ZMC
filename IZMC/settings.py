f2c245cd3b2bc2aaa80f2df7ea6622fbfe9b209f
JethroKakson <jethrokakson@gmail.com>
2024-01-15 17:09:43 +0300

    last commits 7

--- IZMC/settings.py
+++ IZMC/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-&q^ea=a7%u68)k*068uut(@lqszi&a$h0q%y77tn+82-+-0(53')
-
+# SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-&q^ea=a7%u68)k*068uut(@lqszi&a$h0q%y77tn+82-+-0(53')
+SECRET_KEY = 'django-insecure-&q^ea=a7%u68)k*068uut(@lqszi&a$h0q%y77tn+82-+-0(53'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", 'True')=="True"
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'zmc.onrender.com']
 # ALLOWED_HOSTS = []
@@ -80,18 +80,18 @@ WSGI_APPLICATION = 'IZMC.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if not DEBUG:
-    DATABASES = {
+# if not DEBUG:
+DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
         
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
 
 # Password validation
